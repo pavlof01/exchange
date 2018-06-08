0c75d8991b2363902d8db067e98a28bab76b14df
Dmitry Savelev <dsavelev@COMPI012.local>
2018-06-08 14:48:54 +0700

    EXCHRN-22: config gitlab-ci

--- android/app/src/main/java/com/exchange/MainApplication.java
+++ android/app/src/main/java/com/exchange/MainApplication.java
@@ -7,6 +7,8 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 import java.util.Arrays;
 import java.util.List;
@@ -40,6 +42,9 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
+    if(!BuildConfig.DEBUG) {
+        Fabric.with(this, new Crashlytics());
+    }
     SoLoader.init(this, /* native exopackage */ false);
   }
 }
