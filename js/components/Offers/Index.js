8885692879165ee528ead68df8002cfc8b8c8458
vadkou <contact@vadkou.me>
2018-06-25 15:43:17 +0700

    EXCHRN-38 Basic Offers filter connection

--- js/components/Offers/Index.js
+++ js/components/Offers/Index.js
@@ -4,25 +4,113 @@ import PropTypes from 'prop-types';
 import {
     Text,
     View,
-    StyleSheet,
+    StyleSheet, CheckBox, Picker,
 } from 'react-native';
+import FormTextInput from "../FormTextInput";
 
 const styles = StyleSheet.create({
     centerContent: {
         flex: 1,
-        flexDirection: 'row',
+        flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    picker: {
+        height: 50,
+        width: 100,
     },
 });
 
 export default class Offers extends Component {
 
-  render() {
-    return (
-        <View style={styles.centerContent}>
-          <Text>Offers</Text>
-        </View>
-    )
-  }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            actionCode: "buy"
+        }
+    }
+
+    componentDidMount() {
+        this.props.fetchCurrencies();
+        this.props.fetchPaymentMethods();
+        this.props.fetchCountries();
+    }
+
+    onFilterChangeFactory = (name) => (value) => {
+        this.props.updateFilter({ [name]: value });
+    };
+
+    onCryptoCurrencyCodeChange = this.onFilterChangeFactory('cryptoCurrencyCode');
+    onCurrencyCodeChange = this.onFilterChangeFactory('currencyCode');
+    onPaymentMethodCodeChange = this.onFilterChangeFactory('paymentMethodCode');
+    onCountryCodeChange = this.onFilterChangeFactory('countryCode');
+
+
+    onCheckboxChange = (value) => {
+        this.props.updateFilter({
+            'smsRequired': value ? false : null,
+        });
+    };
+
+    render() {
+        return (
+            <View style={styles.centerContent}>
+                {/*{this.actionName}*/}
+
+                <Picker style={styles.picker} selectedValue={this.state.actionCode} onValueChange={(value, index) => this.setState({actionCode: value})} mode={'dropdown'}>
+                    <Picker.Item value="buy" label={'Купить'}/>
+                    <Picker.Item value="sell" label={'Продать'}/>
+                </Picker>
+                {/*{this.props.filter.cryptoCurrencyCode}*/}
+
+                <Picker style={styles.picker} onValueChange={this.onCryptoCurrencyCodeChange} selectedValue={this.props.filter.cryptoCurrencyCode}>
+                    {this.props.cryptoCurrencies.map(
+                        currency => <Picker.Item key={currency.code} value={currency.code} label={currency.code}/>
+                    )}
+                </Picker>
+                {/*{this.props.filter.currencyCode}*/}
+
+                <Picker style={styles.picker} onValueChange={this.onCurrencyCodeChange} selectedValue={this.props.filter.currencyCode}>
+                    {this.props.currencies.map(
+                        currency => <Picker.Item key={currency.code} value={currency.code} label={currency.code}/>
+                    )}
+                </Picker>
+
+
+                {/*{this.props.filter.paymentMethodCode || 'Любым способом'}*/}
+                <Picker style={styles.picker} onValueChange={this.onPaymentMethodCodeChange} selectedValue={this.props.filter.paymentMethodCode || ''}>
+                    <Picker.Item value="ANY" label={'любым способом'} />
+                    {this.props.paymentMethods.map(
+                        method => <Picker.Item key={method.code} value={method.code} label={method.name}/>
+                    )}
+                </Picker>
+
+                {/*в стране*/}
+                {/*{this.props.filter.countryCode}*/}
+                <Picker style={styles.picker} onValueChange={this.onCountryCodeChange} selectedValue={this.props.filter.countryCode}>
+                    {this.props.countries.map(
+                        country => <Picker.Item key={country.code} value={country.code} label={country.name}/>
+                    )}
+                </Picker>
+
+                <FormTextInput placeholder={'Сбербанк, банкомат и пр.'}/>
+                <View>
+                    <CheckBox
+                        value={this.props.filter.smsRequired !== null}
+                        onValueChange={this.onCheckboxChange}/>
+                    {/*Без смс*/}
+                </View>
+
+            </View>
+        )
+    }
 }
+
+Offers.propTypes = {
+    isFetching: PropTypes.bool,
+    updateFilter: PropTypes.func,
+    fetchCurrencies: PropTypes.func,
+    fetchPaymentMethods: PropTypes.func,
+    fetchCountries: PropTypes.func,
+};
--- js/containers/Offers/Index.js
+++ js/containers/Offers/Index.js
@@ -1,10 +1,24 @@
 import { connect } from 'react-redux'
 import Offers from '../../components/Offers/Index'
+import { update as updateFilter } from '../../actions/ordersFilter'
+import { fetch as fetchCurrencies } from '../../actions/currencies'
+import { fetch as fetchPaymentMethods } from '../../actions/paymentMethods'
+import { fetch as fetchCountries } from '../../actions/countries'
 
 const mapDispatchToProps = dispatch => ({
+    updateFilter: (values) => dispatch(updateFilter(values)),
+    fetchCurrencies: (params) => dispatch(fetchCurrencies(dispatch, params)),
+    fetchPaymentMethods: () => dispatch(fetchPaymentMethods(dispatch)),
+    fetchCountries: () => dispatch(fetchCountries(dispatch)),
 });
 
 const mapStateToProps = (state) => ({
+    filter: state.ordersFilter,
+    currencies: state.currencies.list,
+    cryptoCurrencies: state.cryptoCurrencies.list,
+    paymentMethods: state.paymentMethods.list,
+    countries: state.countries.list,
+    orders: state.orders,
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Offers)
