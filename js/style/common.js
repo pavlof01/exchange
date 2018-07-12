82cd145908bf39c5ae4e21c52014acd5f4b14a44
vadkou <contact@vadkou.me>
2018-07-13 01:46:24 +0700

    EXCHRN-66 Added new styling resources (Gilroy font, picker icon)

---
+++ js/style/common.js
@@ -0,0 +1,37 @@
+import {StyleSheet, Text, View} from "react-native";
+import {fonts} from "./resourceHelpers";
+import React from "react";
+
+export const common = StyleSheet.create({
+    content: {
+        flex: 1,
+        paddingTop: 8,
+    },
+    centerContent: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    hintText: {
+        marginTop: 4,
+        marginLeft: 4,
+        fontSize: 11,
+        textAlign: 'left',
+        fontFamily: fonts.medium.regular,
+    },
+    errorText: {
+        flex: 1,
+        color: '#d61b38',
+        textAlign: 'center',
+        fontFamily: fonts.regular.regular,
+    },
+    successText: {
+        flex: 1,
+        color: '#14d459',
+        textAlign: 'center',
+        fontFamily: fonts.regular.regular,
+    },
+});
+
+export const Hint = (props) => <Text {...props} style={common.hintText}/>;
