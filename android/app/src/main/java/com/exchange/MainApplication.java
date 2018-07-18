884b1c4d695b13b9aaf3b6b2a3b171685888da0f
Alexey Nesterov <nesterov@icerockdev.com>
2018-07-18 18:26:57 +0700

    EXCHRN-68 Add one signal package to android application

--- android/app/src/main/java/com/exchange/MainApplication.java
+++ android/app/src/main/java/com/exchange/MainApplication.java
@@ -9,6 +9,7 @@ import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.crashlytics.android.Crashlytics;
 import io.fabric.sdk.android.Fabric;
+import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -24,7 +25,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+          new ReactNativeOneSignalPackage()
       );
     }
 
