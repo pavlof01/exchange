4e7e78d6ec6ee4ac65cb5f047fcdaaaf4bb0dce0
Alexey Nesterov <nesterov@icerockdev.com>
2018-07-27 14:54:23 +0700

    EXCHRN-99 Fix code style in Buy component by eslint config

--- js/components/Trade/Buy.js
+++ js/components/Trade/Buy.js
@@ -1,59 +1,39 @@
-import React, {Component} from "react";
+import React, { Component } from 'react';
 import {
   View,
   Text,
   StyleSheet,
   Image,
-  TouchableOpacity,
   ScrollView,
   Keyboard,
-} from "react-native";
-import ChatView from "./ChatView";
-import Price from "../../values/Price";
-import {currencyCodeToSymbol} from "../../helpers";
-import OnlineStatus from "../../style/OnlineStatus";
-import User from "../../models/User";
-import EscrowTimer from "./EscrowTimer";
-import PrimaryButton from "../../style/ActionButton";
-import moment from "moment";
-import { fonts } from "../../style/resourceHelpers";
+} from 'react-native';
+import moment from 'moment';
+import ChatView from './ChatView';
+import Price from '../../values/Price';
+import User from '../../models/User';
+import EscrowTimer from './EscrowTimer';
+import PrimaryButton from '../../style/ActionButton';
+import { fonts } from '../../style/resourceHelpers';
 import KeyboardAvoidingWrapView from '../KeyboardAvoidingWrapView';
 import TraderInfo from '../TraderInfo';
 import {
+  currencyCodeToSymbol,
   getTradeTitle,
-} from "../../helpers";
+} from '../../helpers';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  row: {
-    flexDirection: "row",
-    alignItems: "center",
-    padding: 8,
-  },
-  header: {
-    color: "#2C09A3",
-    fontWeight: "bold",
-    fontSize: 24,
-    marginBottom: 8,
-  },
-  info: {
-    margin: 5,
-  },
-  centeredText: {
-    textAlign: "center",
-    margin: 8,
-  },
   me: {
     flex: 1,
-    alignItems: "flex-end",
+    alignItems: 'flex-end',
   },
   trader: {
     flex: 1,
   },
   displayNone: {
-    display: "none",
+    display: 'none',
   },
   bottomButtons: {
     paddingBottom: 24,
@@ -134,29 +114,28 @@ const styles = StyleSheet.create({
   },
 });
 
-export default class Buy extends Component {
-
+class Buy extends Component {
   state = {
-    amount: 0,
-    textMessage: "",
+    textMessage: '',
     showKeyboard: false,
     expandChat: true,
     enableScrollViewScroll: true,
   };
 
   _keyboardDidShow = () => {
-    if (!this.state.expandChat) {
-      this.setState({showKeyboard: true});
+    const { expandChat } = this.state;
+    if (!expandChat) {
+      this.setState({ showKeyboard: true });
     }
   };
 
   _keyboardDidHide = () => {
-    this.setState({showKeyboard: false});
+    this.setState({ showKeyboard: false });
   };
 
   componentDidMount() {
-    this.keyboardDidShowListener = Keyboard.addListener("keyboardDidShow", this._keyboardDidShow);
-    this.keyboardDidHideListener = Keyboard.addListener("keyboardDidHide", this._keyboardDidHide);
+    this.keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', this._keyboardDidShow);
+    this.keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', this._keyboardDidHide);
   }
 
   getTradeDescriptionStyleByStatus = (status) => {
@@ -164,35 +143,68 @@ export default class Buy extends Component {
       case 'paid_confirmed':
         return styles.tradeDescriptionConfirmed;
       default:
-        return styles.tradeDescription
+        return styles.tradeDescription;
     }
   };
 
   // TODO: Remove old render message method.
   renderMessage = (message) => {
     const messageUserId = message.item.user.id;
+    const { user } = this.props;
     return (
-      <View style={{paddingLeft: 10, paddingRight: 10, marginTop: 15}} key={messageUserId}>
-        <View style={messageUserId === this.props.user.id ? styles.me : styles.trader}>
-          {messageUserId === this.props.user.id ? null :
-            <Text style={{marginBottom: 10, color: "grey"}}>{message.item.user.userName}</Text>}
-          <View style={{width: "85%", backgroundColor: "#ffffff", padding: 10, flex: 1, borderRadius: 10}}>
-            {message.item.body.length ?
-              (<Text>{message.item.body}</Text>)
-              :
-              (<Image style={{width: 150, height: 150}}
-                      source={{uri: "http://91.228.155.81" + message.item.attachment.url}}/>)}
+      <View style={{ paddingLeft: 10, paddingRight: 10, marginTop: 15 }} key={messageUserId}>
+        <View style={messageUserId === user.id ? styles.me : styles.trader}>
+          {messageUserId === user.id ? null
+            : (
+              <Text style={{ marginBottom: 10, color: 'grey' }}>
+                {message.item.user.userName}
+              </Text>
+            )}
+          <View style={{
+            width: '85%', backgroundColor: '#ffffff', padding: 10, flex: 1, borderRadius: 10,
+          }}
+          >
+            {message.item.body.length
+              ? (
+                <Text>
+                  {message.item.body}
+                </Text>
+              )
+              : (
+                <Image
+                  style={{ width: 150, height: 150 }}
+                  source={{ uri: `http://91.228.155.81${message.item.attachment.url}` }}
+                />
+              )}
           </View>
           <Text
-            style={{fontSize: 12, marginTop: 5, color: "grey"}}>{moment(message.item.date).format("LT")} (MSK)</Text>
+            style={{ fontSize: 12, marginTop: 5, color: 'grey' }}
+          >
+            {moment(message.item.date).format('LT')}
+            {' '}
+            {'(MSK)'}
+          </Text>
         </View>
       </View>
     );
   };
 
   render() {
-    let trade = this.props.trade || {};
-    let ad = trade.ad || {};
+    const {
+      trade,
+      isOnline,
+      partnerName,
+      messages,
+      sendMessage,
+      onCompleteHandler,
+      onCancelHandler,
+    } = this.props;
+    const {
+      enableScrollViewScroll,
+      showKeyboard,
+      textMessage,
+    } = this.state;
+    const { ad } = trade;
     const currencyCode = trade.ad.currency_code || '';
     const cryptoCurrencyCode = trade.ad.crypto_currency_code || '';
     const received = `${Price.build(trade.amount).viewCrypto} ${cryptoCurrencyCode}`;
@@ -204,10 +216,12 @@ export default class Buy extends Component {
       date = paidConfirmedAt.format('DD.MM.YYYY');
       time = `${paidConfirmedAt.format('HH:mm')} (MSK)`;
     } catch (e) {
+      // eslint-disable-next-line no-console
+      console.log(e);
     }
     return (
       <KeyboardAvoidingWrapView
-        behavior={'padding'}
+        behavior="padding"
         style={styles.container}
       >
         <View
@@ -217,66 +231,91 @@ export default class Buy extends Component {
           style={styles.container}
         >
           <ScrollView
-            scrollEnabled={this.state.enableScrollViewScroll}
-            style={{backgroundColor: "#fff", flex: 1}}
-            keyBoardShouldPersistTaps={'never'}
+            scrollEnabled={enableScrollViewScroll}
+            style={{ backgroundColor: '#fff', flex: 1 }}
+            keyBoardShouldPersistTaps="never"
           >
-            <View style={this.state.showKeyboard ? styles.displayNone : null}>
-              <Text style={styles.title}>{getTradeTitle(trade.status, ad.payment_method_code).toUpperCase()}</Text>
+            <View style={showKeyboard ? styles.displayNone : null}>
+              <Text style={styles.title}>
+                {getTradeTitle(trade.status, ad.payment_method_code).toUpperCase()}
+              </Text>
               <TraderInfo
-                isOnline={this.props.isOnline}
-                traderName={this.props.partnerName}
+                isOnline={isOnline}
+                traderName={partnerName}
                 completedTradesCount={User.approximateTradesCount(ad.user.completed_trades_count)}
                 countryCode={ad.country_code}
               />
               <View style={{ paddingStart: 17, paddingEnd: 17 }}>
-              <Text style={styles.costText}>{`1 ${ad.crypto_currency_code} / ${Price.build(ad.price).viewMain} ${currencyCodeToSymbol(ad.currency_code)}`}</Text>
-              <Text style={this.getTradeDescriptionStyleByStatus(trade.status)}>{'Your request Trader '}<Text style={styles.tradeDescriptionBold}>{this.props.partnerName}</Text>{`\nPURCHASE ONLINE cryptocurrency from\n${date} ${time} `}</Text>
-              <View style={styles.swapContainer}>
-                <Text style={styles.swapTextLeft}>{send}</Text>
-                <Image source={require("../../img/ic_swap.png")}
-                       style={{height: 18, width: 18, marginLeft: 15, marginRight: 15}}
-                />
-                <Text style={styles.swapTextRight}>{received}</Text>
+                <Text style={styles.costText}>
+                  {`1 ${ad.crypto_currency_code} / ${Price.build(ad.price).viewMain} ${currencyCodeToSymbol(ad.currency_code)}`}
+                </Text>
+                <Text style={this.getTradeDescriptionStyleByStatus(trade.status)}>
+                  {'Your request Trader '}
+                  <Text style={styles.tradeDescriptionBold}>
+                    {partnerName}
+                  </Text>
+                  {`\nPURCHASE ONLINE cryptocurrency from\n${date} ${time} `}
+                </Text>
+                <View style={styles.swapContainer}>
+                  <Text style={styles.swapTextLeft}>
+                    {send}
+                  </Text>
+                  <Image
+                    source={require('../../img/ic_swap.png')}
+                    style={{
+                      height: 18, width: 18, marginLeft: 15, marginRight: 15,
+                    }}
+                  />
+                  <Text style={styles.swapTextRight}>
+                    {received}
+                  </Text>
+                </View>
+
+                <Text style={styles.timeLeftText}>
+                  {'Time left to pay:'}
+                  <Text style={styles.timeLeftTimeText}>
+                    <EscrowTimer expiredAt={trade.escrow_expired_at} />
+                    {' min'}
+                  </Text>
+                </Text>
+
               </View>
-
-              <Text style={styles.timeLeftText}>Time left to pay: <Text style={styles.timeLeftTimeText}><EscrowTimer expiredAt={this.props.trade.escrow_expired_at}/> min</Text></Text>
-
-            </View>
-            <ChatView
-              onStartShouldSetResponderCapture={
+              <ChatView
+                onStartShouldSetResponderCapture={
                 () => {
                   this.setState({ enableScrollViewScroll: false });
-                  if (this.state.enableScrollViewScroll === false) {
+                  if (enableScrollViewScroll === false) {
                     this.setState({ enableScrollViewScroll: true });
                   }
                 }
               }
-              messages={this.props.messages}
-              userId={this.props.user.id}
-              onChangeText={(textMessage) => this.setState({textMessage})}
-              onSubmitEditing={() => this.props.sendMessage(this.state.textMessage, () => this.setState({textMessage: ""}))}
-              messageValue={this.state.textMessage}
-            />
-            <View style={(this.state.showKeyboard || this.props.trade.status !== 'new') ? styles.displayNone : styles.bottomButtons}>
-              <PrimaryButton
-                onPress={this.props.onCompleteHandler}
-                title={"COMPLETE THE TRANSACTION"}
-                color={"#5B6EFF"}
-                style={{marginTop: 30}}
+                messages={messages}
+                userId={user.id}
+                onChangeText={newTextMessage => this.setState({ textMessage: newTextMessage })}
+                onSubmitEditing={() => sendMessage(textMessage, () => this.setState({ textMessage: '' }))}
+                messageValue={textMessage}
               />
-              <PrimaryButton
-                onPress={this.props.onCancelHandler}
-                title={"CANCEL THE TRANSACTION"}
-                color={"#F5F5F5"}
-                style={{marginTop: 20}}
-                fontStyle={{color: '#000000'}}
-              />
-            </View>
+              <View style={(showKeyboard || trade.status !== 'new') ? styles.displayNone : styles.bottomButtons}>
+                <PrimaryButton
+                  onPress={onCompleteHandler}
+                  title="COMPLETE THE TRANSACTION"
+                  color="#5B6EFF"
+                  style={{ marginTop: 30 }}
+                />
+                <PrimaryButton
+                  onPress={onCancelHandler}
+                  title="CANCEL THE TRANSACTION"
+                  color="#F5F5F5"
+                  style={{ marginTop: 20 }}
+                  fontStyle={{ color: '#000000' }}
+                />
+              </View>
             </View>
           </ScrollView>
         </View>
       </KeyboardAvoidingWrapView>
     );
   }
-};
+}
+
+export default Buy;
