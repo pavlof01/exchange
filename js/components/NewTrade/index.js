dc5c1511b7b4fa24949e2f32edff647e88ff256e
Alexey Nesterov <nesterov@icerockdev.com>
2018-08-02 11:00:26 +0700

    EXCHRN-94 Code style

--- js/components/NewTrade/index.js
+++ js/components/NewTrade/index.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import {
   Text,
@@ -9,166 +9,133 @@ import {
   Image,
   ActivityIndicator,
   TouchableOpacity,
-  TextInput
-} from "react-native";
+  TextInput,
+} from 'react-native';
 import {
   createBasicNavigationOptions,
-  withCommonStatusBar
-} from "../../style/navigation";
-import FormTextInput from "../FormTextInput";
-import Price from "../../values/Price";
-import { currencyCodeToSymbol, objMap } from "../../helpers";
-import PrimaryButton from "../../style/ActionButton";
-import Separator from "../../style/Separator";
-import Api from "../../services/Api";
-import OnlineStatus from "../../style/OnlineStatus";
-import User from "../../models/User";
-import PartnerLink from "../Trade/PartnerLink";
+} from '../../style/navigation';
+import FormTextInput from '../FormTextInput';
+import Price from '../../values/Price';
+import { currencyCodeToSymbol, objMap } from '../../helpers';
+import PrimaryButton from '../../style/ActionButton';
+import Api from '../../services/Api';
+import OnlineStatus from '../../style/OnlineStatus';
+import User from '../../models/User';
 
 const styles = StyleSheet.create({
-  centerContent: {
-    flex: 1,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  row: {
-    flexDirection: "row",
-    alignItems: "center",
-    padding: 8
-  },
   pickerRow: {
-    flexDirection: "row",
-    alignItems: "center",
-    padding: 8
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 8,
   },
   formRow: {
     flex: 1,
-    flexDirection: "row",
-    alignItems: "center"
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   formStyle: {
-    flex: 1
-  },
-  huge: {
-    color: "#222222",
-    fontSize: 26,
-    marginBottom: 8
+    flex: 1,
   },
   header: {
-    color: "#2c09a3",
-    fontWeight: "bold",
+    color: '#2c09a3',
+    fontWeight: 'bold',
     fontSize: 26,
-    marginBottom: 8
+    marginBottom: 8,
   },
   info: {
-    backgroundColor: "white",
+    backgroundColor: 'white',
     margin: 8,
     padding: 8,
-    borderRadius: 4
-  },
-  infoText: {
-    margin: 2,
-    fontSize: 16
-  },
-  bold: {
-    margin: 2,
-    fontSize: 16,
-    fontWeight: "bold"
+    borderRadius: 4,
   },
   centeredText: {
-    textAlign: "center",
+    textAlign: 'center',
     flex: 1,
-    margin: 8
-  },
-  error: {
-    color: "#dd0057",
-    marginBottom: 4
+    margin: 8,
   },
   warning: {
-    color: "#8b572a",
-    backgroundColor: "#fbf5eb",
-    borderColor: "#f5a623",
+    color: '#8b572a',
+    backgroundColor: '#fbf5eb',
+    borderColor: '#f5a623',
     borderRadius: 4,
     borderWidth: 1,
     padding: 8,
-    margin: 8
+    margin: 8,
   },
   amountInputsContainer: {
-    borderColor: "#d5d5d5",
+    borderColor: '#d5d5d5',
     borderBottomWidth: 1,
     flex: 1,
-    flexDirection: "row",
-    //paddingBottom: 10,
-    alignItems: "center"
+    flexDirection: 'row',
+    // paddingBottom: 10,
+    alignItems: 'center',
   },
   currencyCode: {
-    position: "absolute",
+    position: 'absolute',
     right: 0,
-    color: "#4a4a4a",
-    fontWeight: "700",
+    color: '#4a4a4a',
+    fontWeight: '700',
     fontSize: 19,
-    paddingBottom: 10
+    paddingBottom: 10,
   },
   amountText: {
     paddingRight: 50,
     fontSize: 16,
-    paddingBottom: 10
-  }
+    paddingBottom: 10,
+  },
 });
 
 export default class NewTrade extends Component {
-  static navigationOptions = createBasicNavigationOptions("REQUEST");
+  static navigationOptions = createBasicNavigationOptions('REQUEST');
 
   state = {
-    ad: this.props.navigation.getParam("ad", { id: "NO-ID" }),
+    ad: this.props.navigation.getParam('ad', { id: 'NO-ID' }),
     form: {
       amount: undefined,
       cost: undefined,
-      message: undefined
+      message: undefined,
     },
     pending: false,
     errors: undefined,
-    showInfoAboutPartner: false
+    showInfoAboutPartner: false,
   };
 
   componentDidMount() {
     this.setState({ pending: true });
-    Api.get("/pro/" + this.state.ad.id)
+    Api.get(`/pro/${this.state.ad.id}`)
       .then(response => this.setState({ ad: response.data.ad, pending: false }))
-      .catch(error => alert("Ad not found"));
+      .catch(error => alert('Ad not found'));
   }
 
-  onCostChange = value => {
+  onCostChange = (value) => {
     value = value || 0.0;
     const amount = value / this.state.ad.price;
     this.setState({
-      form: { ...this.state.form, cost: value, amount: amount.toFixed(8) }
+      form: { ...this.state.form, cost: value, amount: amount.toFixed(8) },
     });
   };
 
-  onAmountChange = value => {
+  onAmountChange = (value) => {
     value = value || 0.0;
     const cost = value * this.state.ad.price;
     this.setState({
-      form: { ...this.state.form, amount: value, cost: cost.toFixed(2) }
+      form: { ...this.state.form, amount: value, cost: cost.toFixed(2) },
     });
   };
 
-  onMessageChange = value =>
-    this.setState({ form: { ...this.state.form, message: value } });
+  onMessageChange = value => this.setState({ form: { ...this.state.form, message: value } });
 
-  onSubmit = values => {
+  onSubmit = (values) => {
     this.setState({ pending: true, errors: undefined });
     Api.post(`/pro/${this.state.ad.id}/trades`, {
       trade: values,
-      ad: { price: this.state.ad.price }
+      ad: { price: this.state.ad.price },
     })
-      .then(response => {
+      .then((response) => {
         this.setState({ pending: false });
         this.props.openTrade(response.data.trade);
       })
-      .catch(error => {
+      .catch((error) => {
         const newState = { pending: false };
 
         if (error.response.status === 422) {
@@ -178,11 +145,11 @@ export default class NewTrade extends Component {
         } else if (error.response.status === 410) {
           newState.errors = {
             schedule: [
-              "Трейдер в данный момент не работает, смотрите расписание"
-            ]
+              'Трейдер в данный момент не работает, смотрите расписание',
+            ],
           };
         } else if (error.response.status === 405) {
-          newState.errors = { yourself: ["Торговля с собой"] };
+          newState.errors = { yourself: ['Торговля с собой'] };
         }
 
         this.setState(newState);
@@ -195,7 +162,7 @@ export default class NewTrade extends Component {
     curCode,
     isCrypt,
     value,
-    onChange
+    onChange,
   ) {
     const min = Price.build(limitMin);
     const max = Price.build(limitMax);
@@ -206,18 +173,24 @@ export default class NewTrade extends Component {
           <FormTextInput
             keyboardType="numeric"
             style={{ paddingRight: 30, flex: 1 }}
-            placeholder={"0"}
+            placeholder="0"
             value={value}
             onChangeText={onChange}
           />
-          <Text style={styles.currencyCode}>{curCode}</Text>
+          <Text style={styles.currencyCode}>
+            {curCode}
+          </Text>
         </View>
         <Text
-          style={{ marginTop: 10, flex: 1, color: "#4a4a4a", fontSize: 12 }}
+          style={{
+            marginTop: 10, flex: 1, color: '#4a4a4a', fontSize: 12,
+          }}
         >
           Limit:
-          {isCrypt ? min.viewCrypto : min.viewMain} –{" "}
-          {isCrypt ? max.viewCrypto : max.viewMain}{" "}
+          {isCrypt ? min.viewCrypto : min.viewMain}
+          {' - '}
+          {isCrypt ? max.viewCrypto : max.viewMain}
+          {' '}
           {currencyCodeToSymbol(curCode)}
         </Text>
       </View>
@@ -232,7 +205,7 @@ export default class NewTrade extends Component {
       ad.currency_code,
       false,
       form.cost,
-      this.onCostChange
+      this.onCostChange,
     );
   }
 
@@ -244,12 +217,11 @@ export default class NewTrade extends Component {
       ad.crypto_currency_code,
       true,
       form.amount,
-      this.onAmountChange
+      this.onAmountChange,
     );
   }
 
-  showInfoAboutPartner = () =>
-    this.setState({ showInfoAboutPartner: !this.state.showInfoAboutPartner });
+  showInfoAboutPartner = () => this.setState({ showInfoAboutPartner: !this.state.showInfoAboutPartner });
 
   render() {
     const { ad, pending, form } = this.state;
@@ -257,51 +229,53 @@ export default class NewTrade extends Component {
     console.warn(JSON.stringify(ad, null, 2));
     return (
       <ScrollView
-        style={{ padding: 17, backgroundColor: "#fff" }}
-        //keyboardShouldPersistTaps="always"
+        style={{ padding: 17, backgroundColor: '#fff' }}
+        // keyboardShouldPersistTaps="always"
       >
         <View>
           <View
             style={{
-              width: "100%",
+              width: '100%',
               paddingBottom: 6,
               borderBottomWidth: 2,
-              borderColor: "#d5d5d5"
+              borderColor: '#d5d5d5',
             }}
           >
-            <Text style={{ fontSize: 18, color: "grey", fontWeight: "bold" }}>
-              TRANSFER VIA {ad.payment_method_code}
+            <Text style={{ fontSize: 18, color: 'grey', fontWeight: 'bold' }}>
+              TRANSFER VIA
+              {' '}
+              {ad.payment_method_code}
             </Text>
           </View>
           <TouchableOpacity
             onPress={this.showInfoAboutPartner}
             style={{
-              width: "100%",
+              width: '100%',
               marginTop: 15,
-              flexDirection: "row",
-              alignItems: "center",
+              flexDirection: 'row',
+              alignItems: 'center',
               borderBottomWidth: this.state.showInfoAboutPartner ? 0 : 2,
               paddingBottom: 10,
-              borderColor: "#d5d5d5",
-              alignItems: "center"
+              borderColor: '#d5d5d5',
+              alignItems: 'center',
             }}
           >
             <View
               style={{
                 flex: 1,
-                flexDirection: "row",
-                alignItems: "center"
+                flexDirection: 'row',
+                alignItems: 'center',
               }}
             >
               <OnlineStatus isOnline={ad.user.online} />
-              <Text style={{ fontSize: 18, fontWeight: "bold" }}>
+              <Text style={{ fontSize: 18, fontWeight: 'bold' }}>
                 {ad.user.user_name}
               </Text>
               <View
                 style={{
                   marginLeft: 10,
-                  alignItems: "center",
-                  justifyContent: "center"
+                  alignItems: 'center',
+                  justifyContent: 'center',
                 }}
               >
                 <Text>
@@ -311,45 +285,45 @@ export default class NewTrade extends Component {
             </View>
             <View>
               <Image
-                style={{ width: 12, height: 6, justifyContent: "flex-end" }}
-                source={require("../../../assets/icons/Line.png")}
+                style={{ width: 12, height: 6, justifyContent: 'flex-end' }}
+                source={require('../../../assets/icons/Line.png')}
               />
             </View>
           </TouchableOpacity>
           {this.state.showInfoAboutPartner ? (
             <View
               style={{
-                backgroundColor: "#F8F9FB",
+                backgroundColor: '#F8F9FB',
                 paddingBottom: 15,
                 paddingTop: 15,
                 borderBottomWidth: 2,
-                borderColor: "#d5d5d5"
+                borderColor: '#d5d5d5',
               }}
             >
               <Text
                 style={{
-                  color: "#4A4A4A",
+                  color: '#4A4A4A',
                   fontSize: 12,
-                  letterSpacing: 1.5
+                  letterSpacing: 1.5,
                 }}
               >
                 Country
               </Text>
-              <Text style={{ fontSize: 18, marginTop: 10, color: "#4a4a4a" }}>
+              <Text style={{ fontSize: 18, marginTop: 10, color: '#4a4a4a' }}>
                 {ad.country_code}
               </Text>
               <Text
                 style={{
-                  color: "#4A4A4A",
+                  color: '#4A4A4A',
                   fontSize: 12,
                   marginTop: 10,
                   marginBottom: 5,
-                  letterSpacing: 1
+                  letterSpacing: 1,
                 }}
               >
                 Term of transaction
               </Text>
-              <Text style={{ fontSize: 18, color: "#4a4a4a" }}>
+              <Text style={{ fontSize: 18, color: '#4a4a4a' }}>
                 This advertisement is for cash transactions only. Make a request
                 only when you can make a cash payment within 12 hours.
               </Text>
@@ -357,10 +331,10 @@ export default class NewTrade extends Component {
           ) : null}
           <Text
             style={{
-              color: "#4A4A4A",
+              color: '#4A4A4A',
               fontSize: 12,
               marginTop: 16,
-              letterSpacing: 1
+              letterSpacing: 1,
             }}
           >
             COST
@@ -368,17 +342,22 @@ export default class NewTrade extends Component {
           <View style={styles.info}>
             <Text style={styles.centeredText}>
               <Text style={styles.header}>
-                1 {ad.crypto_currency_code} / {Price.build(ad.price).viewMain}{" "}
+                1
+                {' '}
+                {ad.crypto_currency_code}
+                {' / '}
+                {Price.build(ad.price).viewMain}
+                {' '}
                 {currencyCodeToSymbol(ad.currency_code)}
               </Text>
             </Text>
           </View>
           <Text
             style={{
-              color: "#4A4A4A",
+              color: '#4A4A4A',
               fontSize: 12,
               marginBottom: 10,
-              letterSpacing: 1
+              letterSpacing: 1,
             }}
           >
             AMOUNT
@@ -387,18 +366,18 @@ export default class NewTrade extends Component {
           <View style={styles.pickerRow}>
             {this.renderFiatCurrencyInput()}
             <Image
-              source={require("../../img/ic_swap.png")}
+              source={require('../../img/ic_swap.png')}
               style={[styles.pickerIcon, { margin: 16 }]}
             />
             {this.renderCryptoCurrencyInput()}
           </View>
           <Text
             style={{
-              color: "#4A4A4A",
+              color: '#4A4A4A',
               fontSize: 12,
               marginTop: 30,
               marginBottom: 10,
-              letterSpacing: 1
+              letterSpacing: 1,
             }}
           >
             MESSAGE
@@ -415,25 +394,27 @@ export default class NewTrade extends Component {
           <View
             style={{
               marginTop: 20,
-              flexDirection: "row",
-              flex: 1
+              flexDirection: 'row',
+              flex: 1,
             }}
           >
-            <Text style={{ color: "#4a4a4a" }}>
+            <Text style={{ color: '#4a4a4a' }}>
               Time limit for payment of seller's invoice:
             </Text>
-            <Text style={{ fontWeight: "700" }}> 90 min</Text>
+            <Text style={{ fontWeight: '700' }}>
+              {' 90 min' /* TODO: Get time value */ }
+            </Text>
           </View>
 
           <PrimaryButton
             onPress={() => this.onSubmit(form)}
-            title={"SEND A REQUSET TO TRADER"}
+            title="SEND A REQUSET TO TRADER"
             disabled={pending}
             style={{
               marginTop: 30,
               flex: 1,
-              backgroundColor: "#5b6eff",
-              boxShadow: "0 3 5 -3 rgba(31, 89, 230, 0.8)"
+              backgroundColor: '#5b6eff',
+              boxShadow: '0 3 5 -3 rgba(31, 89, 230, 0.8)',
             }}
           >
             {pending ? <ActivityIndicator size="large" /> : undefined}
@@ -441,7 +422,9 @@ export default class NewTrade extends Component {
 
           {objMap(this.state.errors, (key, value) => (
             <Text style={styles.warning} key={key}>
-              {key}: {value.join(". ")}
+              {key}
+:
+              {value.join('. ')}
             </Text>
           ))}
         </View>
@@ -452,5 +435,5 @@ export default class NewTrade extends Component {
 
 NewTrade.propTypes = {
   isFetching: PropTypes.bool,
-  openTrade: PropTypes.func
+  openTrade: PropTypes.func,
 };
