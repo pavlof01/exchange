42d8a4ba417944be20c14cf802fac4efe62bd843
Alexey Nesterov <nesterov@icerockdev.com>
2018-08-03 18:36:55 +0700

    EXCHRN-139 Code style

--- js/style/CardPicker/index.js
+++ js/style/CardPicker/index.js
@@ -1,85 +1,83 @@
 import React from 'react';
 import {
-    StyleSheet, Text,
-    View,
+  StyleSheet,
 } from 'react-native';
 import {
-    Menu,
-    MenuOptions,
-    MenuTrigger,
+  Menu,
+  MenuOptions,
+  MenuTrigger,
 } from 'react-native-popup-menu';
 import PropTypes from 'prop-types';
 
 const styles = StyleSheet.create({
-    picker: {
-        elevation: 4,
-        borderRadius: 4,
-        backgroundColor: 'white',
-        margin: 8,
-        shadowOffset: {
-            width: 0,
-            height: 2
-        },
-        shadowRadius: 3,
-        shadowOpacity: 0.5,
+  picker: {
+    elevation: 4,
+    borderRadius: 4,
+    backgroundColor: 'white',
+    margin: 8,
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
-    cardText: {
-        fontSize: 24,
-        color: 'black',
-    }
+    shadowRadius: 3,
+    shadowOpacity: 0.5,
+  },
+  cardText: {
+    fontSize: 24,
+    color: 'black',
+  },
 });
 
 class CardPicker extends React.Component {
-
-
     static propTypes = {
-        /**
+      /**
          * Callback on item selection.
          */
-        onValueChange: PropTypes.func,
+      onValueChange: PropTypes.func,
 
-        /**
+      /**
          * First selected item
          */
-        selectedValue: PropTypes.string,
+      selectedValue: PropTypes.string,
 
-        renderButton: PropTypes.func,
+      renderButton: PropTypes.func,
 
-        fontSize: PropTypes.number,
+      fontSize: PropTypes.number,
 
-        flat: PropTypes.bool,
+      flat: PropTypes.bool,
     };
 
     state = {
-        selectedValue: this.props.selectedValue,
+      selectedValue: this.props.selectedValue,
     };
 
     handleChange = (newValue) => {
-        this.props.onValueChange(newValue);
-        this.setState({ selectedValue: newValue });
+      this.props.onValueChange(newValue);
+      this.setState({ selectedValue: newValue });
     };
 
     render() {
-        const { fontSize, flat } = this.props;
+      const { fontSize, flat } = this.props;
 
-        let selected;
-        const options = React.Children.map(this.props.children,
-            (child) => {
-                if(child.props.value === this.state.selectedValue) {
-                    selected = child;
-                }
-                return React.cloneElement(child, {onSelect: this.handleChange, customStyles: {optionText: [styles.cardText, fontSize ? {fontSize} : undefined]}})
-            }
-        );
+      let selected;
+      const options = React.Children.map(this.props.children,
+        (child) => {
+          if (child.props.value === this.state.selectedValue) {
+            selected = child;
+          }
+          return React.cloneElement(child, { onSelect: this.handleChange, customStyles: { optionText: [styles.cardText, fontSize ? { fontSize } : undefined] } });
+        });
 
-        return (
-            <Menu>
-                <MenuTrigger customStyles={!flat ? {triggerOuterWrapper: styles.picker} : undefined}>{this.props.renderButton(this.state.selectedValue, selected && selected.props.text)}</MenuTrigger>
-                <MenuOptions>
-                    {options}
-                </MenuOptions>
-            </Menu>
-        );
+      return (
+        <Menu>
+          <MenuTrigger customStyles={!flat ? { triggerOuterWrapper: styles.picker } : undefined}>
+            {this.props.renderButton(this.state.selectedValue, selected && selected.props.text)}
+          </MenuTrigger>
+          <MenuOptions>
+            {options}
+          </MenuOptions>
+        </Menu>
+      );
     }
 }
 
