c549e03e791bdc7e9f3868277093a5c1ebdcc86d
Aleksey Pavlov <apavlov@icerockdev.com>
2018-08-08 17:34:40 +0700

    EXCHRN-124 enter pincode before login

--- js/components/PincodeAutorization/index.js
+++ js/components/PincodeAutorization/index.js
@@ -1,22 +1,210 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import {
+  View, Text, StyleSheet, Image, TouchableOpacity, AsyncStorage,
+} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import ModalDialog from '../../style/ModalDialog';
 
-export default class PincodeAutorization extends Component {
+export default class PincodeAurorization extends Component {
+  static navigationOptions = { header: props => null };
   constructor(props) {
     super(props);
     this.state = {
+      pincode: '',
+      rightPincode: false
     };
+  }
+
+  checkPasscode = async () => {
+    const { initialRoute } = this.props;
+    const passcode = await AsyncStorage.getItem('pincode');
+    if (passcode === this.state.pincode) {
+      initialRoute();
+    } else {
+      // TODO: ЗАПУСТИТЬ АНИМАЦИЮ ПРИ НЕПРАВИЛЬНОМ ВВОДЕ ПИНКОДА
+      console.warn("no");
+    }
+  }
+
+  setPincode = (num) => {
+    if (this.state.pincode.length < 4) {
+      let pincode = this.state.pincode;
+      pincode += num;
+      this.setState({ pincode }, () => {
+        if (this.state.pincode.length >= 4) {
+          this.checkPasscode();
+        }
+      });
+    }
+  }
+
+  delete = () => {
+    if (this.state.pincode.length > 0 < 4) {
+      const pincode = String(this.state.pincode);
+      const newPin = pincode.slice(0, -1);
+      this.setState({ pincode: newPin });
+    }
   }
 
   render() {
     return (
-      <View>
-        <Text>
-          {' '}
-          PincodeAutorization
-          {' '}
+      <LinearGradient
+        colors={['#3F579E', '#426CA6', '#426CA6', '#384D8C', '#203057']}
+        style={[styles.paddingScreen]}
+      >
+        <Image style={styles.logo} source={require('../../img/bitpapa.png')} />
+        <Text style={styles.pincode}>
+          {this.state.confirm ? 'CONFIRM PINCODE' : 'PIN CODE'}
         </Text>
-      </View>
+        <View style={styles.circleContainer}>
+          <View style={this.state.pincode.length > 0 ? styles.circleFill : this.state.rightPincode ? style.circleRight : styles.circle} />
+          <View style={this.state.pincode.length > 1 ? styles.circleFill : styles.circle} />
+          <View style={this.state.pincode.length > 2 ? styles.circleFill : styles.circle} />
+          <View style={this.state.pincode.length > 3 ? styles.circleFill : styles.circle} />
+        </View>
+        <View style={styles.numbersContainer}>
+          <View style={styles.row}>
+            <TouchableOpacity onPress={() => this.setPincode('1')} style={styles.circleNumber}>
+              <Text style={styles.numberText}>
+                1
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => this.setPincode('2')} style={styles.circleNumber}>
+              <Text style={styles.numberText}>
+                2
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => this.setPincode('3')} style={styles.circleNumber}>
+              <Text style={styles.numberText}>
+                3
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.row}>
+            <TouchableOpacity onPress={() => this.setPincode('4')} style={styles.circleNumber}>
+              <Text style={styles.numberText}>
+                4
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => this.setPincode('5')} style={styles.circleNumber}>
+              <Text style={styles.numberText}>
+                5
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => this.setPincode('6')} style={styles.circleNumber}>
+              <Text style={styles.numberText}>
+                6
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.row}>
+            <TouchableOpacity onPress={() => this.setPincode('7')} style={styles.circleNumber}>
+              <Text style={styles.numberText}>
+                7
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => this.setPincode('8')} style={styles.circleNumber}>
+              <Text style={styles.numberText}>
+                8
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => this.setPincode('9')} style={styles.circleNumber}>
+              <Text style={styles.numberText}>
+                9
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.row}>
+            <View style={styles.empty} />
+            <TouchableOpacity onPress={() => this.setPincode('0')} style={styles.circleNumber}>
+              <Text style={styles.numberText}>
+                0
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={this.delete} style={styles.delete}>
+              <Image source={require('../../img/delete.png')} />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </LinearGradient>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  paddingScreen: {
+    padding: 42,
+    flexDirection: 'column',
+    flex: 1,
+  },
+  logo: {
+    alignSelf: 'center',
+    marginTop: 20,
+  },
+  pincode: {
+    textAlign: 'center',
+    color: '#f2f6f9',
+    fontSize: 18,
+    fontWeight: '400',
+    marginTop: 30,
+  },
+  circleContainer: {
+    flexDirection: 'row',
+    width: 150,
+    justifyContent: 'space-between',
+    alignSelf: 'center',
+    marginTop: 30,
+  },
+  circle: {
+    width: 20,
+    height: 20,
+    backgroundColor: '#94b7ff',
+    borderRadius: 10,
+  },
+  circleFill: {
+    width: 20,
+    height: 20,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+  },
+  circleRight: {
+    width: 20,
+    height: 20,
+    backgroundColor: 'green',
+    borderRadius: 10,
+  },
+  numbersContainer: {
+    marginTop: 30,
+    flex: 1,
+    alignItems: 'center',
+  },
+  row: {
+    flexDirection: 'row',
+  },
+  circleNumber: {
+    width: 70,
+    height: 70,
+    backgroundColor: 'rgba(148, 183, 255, 0.15)',
+    borderRadius: 35,
+    margin: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  empty: {
+    width: 70,
+    height: 70,
+    margin: 10,
+  },
+  delete: {
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+    margin: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  numberText: {
+    color: '#ffffff',
+    fontSize: 36,
+  },
+});
