531e1c0ba8fd23d3c01a6f9e9f68734a7e270698
Aleksey Pavlov <apavlov@icerockdev.com>
2018-08-22 16:31:27 +0700

    EXCHRN-160 Root.js eslint fixed

--- js/Root.js
+++ js/Root.js
@@ -1,21 +1,20 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
+import { MenuProvider } from 'react-native-popup-menu';
 import store from './store';
 
 import App from './App';
-import {MenuProvider} from "react-native-popup-menu";
 
 class Root extends Component {
-
-    render() {
-        return (
-            <MenuProvider>
-                <Provider store={store}>
-                    <App />
-                </Provider>
-            </MenuProvider>
-        );
-    }
+  render() {
+    return (
+      <MenuProvider>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </MenuProvider>
+    );
+  }
 }
 
 export default Root;
