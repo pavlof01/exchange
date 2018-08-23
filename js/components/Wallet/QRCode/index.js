929819d8e031b6eb079de479493a19be31285167
Aleksey Pavlov <apavlov@icerockdev.com>
2018-08-23 18:02:43 +0700

    EXCHRN-160 QRCode eslint fixed

--- js/components/Wallet/QRCode/index.js
+++ js/components/Wallet/QRCode/index.js
@@ -2,54 +2,55 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import {
-    ActivityIndicator,
-    Text,
-    Image,
-    View,
-    StyleSheet,
+  ActivityIndicator,
+  Text,
+  Image,
+  View,
+  StyleSheet,
 } from 'react-native';
 
 const styles = StyleSheet.create({
-    centerContent: {
-        flex: 1,
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-        minHeight: 276,
-        padding: 8,
-    },
+  centerContent: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    minHeight: 276,
+    padding: 8,
+  },
 });
 
 export default class QRCode extends Component {
+  static propTypes = {
+    transactionTokens: PropTypes.any, // eslint-disable-line react/forbid-prop-types
+  };
 
-    static propTypes = {
-        transactionTokens: PropTypes.any,
-    };
+  render() {
+    const { transactionTokens } = this.props;
 
-    render() {
-        const { transactionTokens } = this.props;
-
-        let content = null;
-        if (transactionTokens && transactionTokens.generation_pending) {
-            content = (<ActivityIndicator size="large" />);
-        } else if (transactionTokens && transactionTokens.data && transactionTokens.data.length > 0) {
-            const base64code = transactionTokens.data[0].qr_code.replace(/\n/g, '');
-            content = (
-                <Image
-                    source={{uri: `data:image/png;base64, ${base64code}`}}
-                    style={{width: 260, height: 260}}
-                />
-            );
-        } else {
-            content = (
-                <Text>You don't have any active addresses</Text>
-            );
-        }
-
-        return (
-            <View style={styles.centerContent}>
-                { content }
-            </View>
-        )
+    let content = null;
+    if (transactionTokens && transactionTokens.generation_pending) {
+      content = (<ActivityIndicator size="large" />);
+    } else if (transactionTokens && transactionTokens.data && transactionTokens.data.length > 0) {
+      const base64code = transactionTokens.data[0].qr_code.replace(/\n/g, '');
+      content = (
+        <Image
+          source={{ uri: `data:image/png;base64, ${base64code}` }}
+          style={{ width: 260, height: 260 }}
+        />
+      );
+    } else {
+      content = (
+        <Text>
+          You don't have any active addresses
+        </Text>
+      );
     }
+
+    return (
+      <View style={styles.centerContent}>
+        {content}
+      </View>
+    );
+  }
 }
