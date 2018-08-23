91e8aa20a20a7f4bff57ebb05c83bb60efb3aed1
Aleksey Pavlov <apavlov@icerockdev.com>
2018-08-23 17:00:55 +0700

    EXCHRN-160 containers eslint fixed

--- js/containers/Ads/Index.js
+++ js/containers/Ads/Index.js
@@ -1,12 +1,4 @@
-import { connect } from 'react-redux'
-import Ads from '../../components/Ads'
+import { connect } from 'react-redux';
+import Ads from '../../components/Ads';
 
-const mapDispatchToProps = dispatch => ({
-    
-});
-
-const mapStateToProps = (state) => ({
-
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Ads)
+export default connect(null, null)(Ads);
--- js/containers/Login/Index.js
+++ js/containers/Login/Index.js
@@ -1,26 +1,26 @@
-import {connect} from 'react-redux'
-import Login from '../../components/Login/Index'
-import {fetchDictionary} from '../../actions/i18n';
-import {signUpRequest} from '../../actions/signUp';
-import {recoverPasswordRequest} from '../../actions/recoverPassword';
-import {login} from "../../actions/session";
+import { connect } from 'react-redux';
+import Login from '../../components/Login/Index';
+import { fetchDictionary } from '../../actions/i18n';
+import { signUpRequest } from '../../actions/signUp';
+import { recoverPasswordRequest } from '../../actions/recoverPassword';
+import { login } from '../../actions/session';
 
 const mapDispatchToProps = dispatch => ({
-    fetchDictionary: () => dispatch(fetchDictionary(dispatch)),
-    login: (params) => {
-        dispatch(login(params))
-    },
-    signUpRequest: () => {
-        dispatch(signUpRequest())
-    },
-    recoverPasswordRequest: (params) => {
-        dispatch(recoverPasswordRequest(params))
-    }
+  fetchDictionary: () => dispatch(fetchDictionary(dispatch)),
+  login: (params) => {
+    dispatch(login(params));
+  },
+  signUpRequest: () => {
+    dispatch(signUpRequest());
+  },
+  recoverPasswordRequest: (params) => {
+    dispatch(recoverPasswordRequest(params));
+  },
 });
 
-const mapStateToProps = (state) => ({
-    isFetching: state.session.pending,
-    formError: state.session.error,
+const mapStateToProps = state => ({
+  isFetching: state.session.pending,
+  formError: state.session.error,
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(Login)
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
