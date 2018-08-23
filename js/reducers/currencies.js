753da494433d1fb48ce1678deece828fc96089c4
Aleksey Pavlov <apavlov@icerockdev.com>
2018-08-23 16:28:13 +0700

    EXCHRN-160 reducer currencies.js eslint fixed

--- js/reducers/currencies.js
+++ js/reducers/currencies.js
@@ -1,4 +1,4 @@
-import { CURRENCIES } from '../actions'
+import { CURRENCIES } from '../actions';
 
 const initial = {
   list: [],
@@ -6,24 +6,24 @@ const initial = {
 
 const currencyFilter = new Set();
 
-currencyFilter.add("CNY");
-currencyFilter.add("EUR");
-currencyFilter.add("GBP");
-currencyFilter.add("USD");
-currencyFilter.add("RUB");
+currencyFilter.add('CNY');
+currencyFilter.add('EUR');
+currencyFilter.add('GBP');
+currencyFilter.add('USD');
+currencyFilter.add('RUB');
 
 export default (state = initial, action) => {
   switch (action.type) {
     case CURRENCIES.FETCH_CURRENCIES_STARTED: return {
-      ...state, pending: true
+      ...state, pending: true,
     };
 
     case CURRENCIES.FETCH_CURRENCIES_SUCCEED: return {
-      ...state, pending: false, list: action.currencies.filter((cur) => currencyFilter.has(cur.code))
+      ...state, pending: false, list: action.currencies.filter(cur => currencyFilter.has(cur.code)),
     };
 
     case CURRENCIES.FETCH_CURRENCIES_FAILURE: return {
-      ...state, pending: false
+      ...state, pending: false,
     };
 
     default: return state;
