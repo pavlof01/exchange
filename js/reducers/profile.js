f4927b00edb81375b3065af019fe73abec8de5af
Aleksey Pavlov <apavlov@icerockdev.com>
2018-08-23 16:30:25 +0700

    EXCHRN-160 reducer profile.js eslint fixed

--- js/reducers/profile.js
+++ js/reducers/profile.js
@@ -1,50 +1,51 @@
-import { PROFILE } from '../actions'
+import { PROFILE } from '../actions';
 
 const initial = {
-    feedbacks: {
-        page: 1,
-        data: [],
-        error: null,
-        pending: false,
-        total_pages: null,
+  feedbacks: {
+    page: 1,
+    data: [],
+    error: null,
+    pending: false,
+    total_pages: null,
 
-        different_users_count: 0,
-        positive_count: 0,
-        block_count: 0,
-        neutral_count : 0,
-        total_count   : 0,
-    }
+    different_users_count: 0,
+    positive_count: 0,
+    block_count: 0,
+    neutral_count: 0,
+    total_count: 0,
+  },
 };
 
 export default (state = initial, action) => {
-    switch (action.type) {
-        case PROFILE.FETCH_FEEDBACK_STARTED: return {
-            ...state, feedbacks: { ...state.feedbacks, error: null, pending: true }
-        };
+  switch (action.type) {
+    case PROFILE.FETCH_FEEDBACK_STARTED: return {
+      ...state, feedbacks: { ...state.feedbacks, error: null, pending: true },
+    };
 
-        case PROFILE.FETCH_FEEDBACK_SUCCEED: return {
-            ...state, feedbacks: {
-                ...state.feedbacks,
+    case PROFILE.FETCH_FEEDBACK_SUCCEED: return {
+      ...state,
+      feedbacks: {
+        ...state.feedbacks,
 
-                total_pages: action.data.total_pages,
-                data: action.data.page === 1 ? action.data.feedbacks : [...state.feedbacks.data, ...action.data.feedbacks],
-                page: action.data.page,
+        total_pages: action.data.total_pages,
+        data: action.data.page === 1 ? action.data.feedbacks : [...state.feedbacks.data, ...action.data.feedbacks],
+        page: action.data.page,
 
-                different_users_count: action.data.different_users_count,
-                all_count: action.data.all_count,
-                positive_count: action.data.positive_count,
-                block_count: action.data.block_count,
-                neutral_count : action.data.neutral_count,
-                total_count   : action.data.total_count,
+        different_users_count: action.data.different_users_count,
+        all_count: action.data.all_count,
+        positive_count: action.data.positive_count,
+        block_count: action.data.block_count,
+        neutral_count: action.data.neutral_count,
+        total_count: action.data.total_count,
 
-                pending: false
-            }
-        };
+        pending: false,
+      },
+    };
 
-        case PROFILE.FETCH_FEEDBACK_FAILURE: return {
-            ...state, feedbacks: { ...state.feedbacks, error: action.error, pending: false }
-        };
+    case PROFILE.FETCH_FEEDBACK_FAILURE: return {
+      ...state, feedbacks: { ...state.feedbacks, error: action.error, pending: false },
+    };
 
-        default: return state
-    }
-}
+    default: return state;
+  }
+};
