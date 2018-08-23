81a2cae964a0482c7b624605cd77a34b2d647b51
Aleksey Pavlov <apavlov@icerockdev.com>
2018-08-23 16:54:39 +0700

    EXCHRN-160 middlewares eslint fixed

--- js/middlewares/orders.js
+++ js/middlewares/orders.js
@@ -1,10 +1,10 @@
-import {ORDERS_FILTER, SESSION} from '../actions'
-import { fetch } from '../actions/orders'
+import { ORDERS_FILTER } from '../actions';
+import { fetch } from '../actions/orders';
 
-export default store => next => action => {
+export default store => next => (action) => {
   if (action.type === ORDERS_FILTER.UPDATE_ORDERS_FILTER) {
-    store.dispatch(fetch(store.dispatch, {...store.getState().ordersFilter, ...action.values}));
+    store.dispatch(fetch(store.dispatch, { ...store.getState().ordersFilter, ...action.values }));
   }
 
   next(action);
-}
+};
