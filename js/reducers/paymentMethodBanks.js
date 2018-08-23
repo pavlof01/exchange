203d6adc1c4fe7c43e99f06298a92d1dddc0c86c
Aleksey Pavlov <apavlov@icerockdev.com>
2018-08-23 16:30:02 +0700

    EXCHRN-160 reducer PaymentMethodeBanks.js eslint fixed

--- js/reducers/paymentMethodBanks.js
+++ js/reducers/paymentMethodBanks.js
@@ -1,21 +1,21 @@
-import { PAYMENT_METHOD_BANKS } from '../actions'
+import { PAYMENT_METHOD_BANKS } from '../actions';
 
 const initial = {
-    list: [],
+  list: [],
 };
 
 export default (state = initial, action) => {
   switch (action.type) {
     case PAYMENT_METHOD_BANKS.FETCH_PAYMENT_METHOD_BANKS_STARTED: return {
-      ...state, pending: true
+      ...state, pending: true,
     };
 
     case PAYMENT_METHOD_BANKS.FETCH_PAYMENT_METHOD_BANKS_SUCCEED: return {
-      ...state, pending: false, list: action.banks
+      ...state, pending: false, list: action.banks,
     };
 
     case PAYMENT_METHOD_BANKS.FETCH_PAYMENT_METHOD_BANKS_FAILURE: return {
-      ...state, pending: false
+      ...state, pending: false,
     };
 
     default: return state;
