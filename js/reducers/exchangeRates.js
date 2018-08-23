cf8c9dcd76eb3d84c53d2e8568e686db44ffd9ab
Aleksey Pavlov <apavlov@icerockdev.com>
2018-08-23 16:28:40 +0700

    EXCHRN-160 reducer exchangeRates.js eslint fixed

--- js/reducers/exchangeRates.js
+++ js/reducers/exchangeRates.js
@@ -1,23 +1,23 @@
-import { EXCHANGE_RATES } from '../actions'
+import { EXCHANGE_RATES } from '../actions';
 
 const initial = {
   pending: false,
 };
 
 export default (state = initial, action) => {
-  let {type, ...rates} = action;
+  const { type, ...rates } = action;
 
   switch (type) {
     case EXCHANGE_RATES.FETCH_EXCHANGE_RATES_STARTED: return {
-      ...state, pending: true
+      ...state, pending: true,
     };
 
     case EXCHANGE_RATES.FETCH_EXCHANGE_RATES_SUCCEED: return {
-      ...state, ...rates, pending: false
+      ...state, ...rates, pending: false,
     };
 
     case EXCHANGE_RATES.FETCH_EXCHANGE_RATES_FAILURE: return {
-      ...state, pending: false
+      ...state, pending: false,
     };
 
     default: return state;
