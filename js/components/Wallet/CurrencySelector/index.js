c0394246ba6d81e3aef7e0af8573dc27c766dd74
Aleksey Pavlov <apavlov@icerockdev.com>
2018-09-12 12:45:33 +0700

    EXCHRN-223 переделана иконка выбора валюты, экран трансфер

--- js/components/Wallet/CurrencySelector/index.js
+++ js/components/Wallet/CurrencySelector/index.js
@@ -30,14 +30,23 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.8,
     shadowRadius: 2,
     justifyContent: 'space-between',
-    opacity: 0.2,
+    opacity: 0.1,
     elevation: 4,
-    borderRadius: 4,
+    borderRadius: 8,
+    borderColor: 'rgba(255,255,255, 0.5)',
+    borderWidth: 1,
+  },
+  bitcoinIconContainer: {
+    alignSelf: 'flex-end',
+    width: 38,
+    height: 38,
+    borderRadius: 19,
+    backgroundColor: '#fa9925',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   cryptImage: {
-    alignSelf: 'flex-end',
-    width: 37,
-    height: 37,
+
   },
   cryptNameText: {
     color: '#fff',
@@ -48,8 +57,14 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: '600',
   },
-  active: {
+  noActiveIcon: {
+    backgroundColor: '#25367e',
+    borderColor: 'rgba(255,255,255, 0.5)',
+    borderWidth: 1,
+  },
+  setOpacity: {
     opacity: 1,
+    borderColor: 'rgba(255,255,255, 0)',
   },
 });
 
@@ -75,8 +90,10 @@ export default class CurrencySelector extends Component {
     return (
       <View style={styles.cryptHeader}>
         <Touchable onPress={() => this.onCryptoCurrencyCodeChange('BTC')}>
-          <View style={[styles.cryptContainer, this.state.cryptoCurrencyCode === 'BTC' ? styles.active : null]}>
-            <Image resizeMode="contain" style={styles.cryptImage} source={require('../../../img/ic_btc.png')} />
+          <View style={[styles.cryptContainer, this.state.cryptoCurrencyCode === 'BTC' ? styles.setOpacity : null]}>
+            <View style={[styles.bitcoinIconContainer, this.state.cryptoCurrencyCode !== 'BTC' ? styles.noActiveIcon : null]}>
+              <Image resizeMode="contain" style={styles.cryptImage} source={require('../../../img/btc_icon.png')} />
+            </View>
             <View>
               <Text style={styles.cryptNameText}>
                 BTC
@@ -88,8 +105,10 @@ export default class CurrencySelector extends Component {
           </View>
         </Touchable>
         <Touchable onPress={() => this.onCryptoCurrencyCodeChange('ETH')}>
-          <View style={[styles.cryptContainer, this.state.cryptoCurrencyCode === 'ETH' ? styles.active : null]}>
-            <Image resizeMode="contain" style={styles.cryptImage} source={require('../../../img/ic_eth.png')} />
+          <View style={[styles.cryptContainer, this.state.cryptoCurrencyCode === 'ETH' ? styles.setOpacity : null]}>
+            <View style={[styles.bitcoinIconContainer, { backgroundColor: '#487393' }, this.state.cryptoCurrencyCode !== 'ETH' ? styles.noActiveIcon : null]}>
+              <Image resizeMode="contain" style={styles.cryptImage} source={require('../../../img/eth_icon.png')} />
+            </View>
             <View>
               <Text style={styles.cryptNameText}>
                 ETH
@@ -100,7 +119,7 @@ export default class CurrencySelector extends Component {
             </View>
           </View>
         </Touchable>
-      </View>
+      </View >
     );
   }
 }
