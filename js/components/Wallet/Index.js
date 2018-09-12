0e9b7217db81509bc50fe499a269c1e8081611b7
Aleksey Pavlov <apavlov@icerockdev.com>
2018-09-12 11:42:22 +0700

    EXCHRN-223 убрана белая полоса и изменен тайтл в хедере

--- js/components/Wallet/Index.js
+++ js/components/Wallet/Index.js
@@ -15,7 +15,7 @@ import ConfirmDialog from './ConfirmDialog';
 
 const isAndroid = Platform.OS === 'android';
 
-const MARGIN_BETWEEN_HEADER_AND_ABSOLUTE_CONTAINER = isAndroid ? 140 : 149;
+const MARGIN_BETWEEN_HEADER_AND_ABSOLUTE_CONTAINER = isAndroid ? 137 : 149;
 const MARGIN_BETWEEN_HEADER_BAR_AND_BUTTONS = 20;
 
 const styles = StyleSheet.create({
@@ -172,7 +172,7 @@ class Wallet extends Component {
       <ScrollView bounces={false} style={styles.safeContainer}>
         <View style={styles.container}>
           <HeaderBar
-            title={header}
+            title={intl.formatMessage({ id: 'app.wallet.title.transfer', defaultMessage: 'Transfer' }).toUpperCase()}
             rightIcon={<Image resizeMode="contain" style={styles.rightIconOfHeaderBar} source={require('../../img/transactions.png')} />}
             onPress={() => this.props.openTransactions()}
           />
