e784d459c0ad5d7ee3c35eb4892d3a4d7381fb71
Aleksey Pavlov <apavlov@icerockdev.com>
2018-09-17 14:40:25 +0700

    EXCHRN-239 изменение курса крипты переведено в проценты

--- js/components/Offers/index.js
+++ js/components/Offers/index.js
@@ -716,7 +716,10 @@ class Offers extends React.PureComponent {
       exchangeRates,
     } = this.props;
     if (exchangeRates.rates) {
-      return `${exchangeRates.rates[`change_${hours}${time}`].toFixed(2)}`;
+      const currencyRate = exchangeRates.rate;
+      const changeInCurrency = `${exchangeRates.rates[`change_${hours}${time}`].toFixed(2)}`;
+      const changeInPercent = (changeInCurrency / currencyRate) * 100;
+      return `${changeInPercent.toFixed(2)} %`;
     }
     return '';
   };
